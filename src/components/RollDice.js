7e865d5c1592f7ad9142bd1409a0bd937d2b8d61
Mutombo <mgtshomba@gmail.com>
2020-11-16 14:34:37 +0200

    Adding the state to the componment

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import Die from './Die'
 
  class RollDice extends Component {
+     constructor(props){
+         super(props)
+         this.state = {
+             dice1 : 1,
+             dice2 : 6
+         }
+     }
     render() {
         return (
             <div>
