387796bf5af8ed733a7640987c18bf3b62b47138
Mutombo <mgtshomba@gmail.com>
2020-11-16 15:50:06 +0200

    added a switch stament

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -6,18 +6,46 @@ import Die from './Die'
          super(props)
          this.state = {
              dice1 : 1,
-             dice2 : 6
+             dice2 : 6,
          }
     }
 
     rollDice = ()=>{
         // Generate a random number for the first and second dice 1-6
-        let diceNum1 = Math.floor(Math.random() * 6) + 1
-        let diceNum2 = Math.floor(Math.random() * 6) + 1
+        let diceNum1 = Math.floor(Math.random() * 6) + 1;
+        let diceNum2 = Math.floor(Math.random() * 6) + 1;
+
         // Set the first dice to the generated number
         this.setState({dice1: diceNum1})
         this.setState({dice2: diceNum2})
-        console.log(this.state.dice1);
+
+        // Check value of Rolled Dice
+        let dice;
+        switch(diceNum1){
+            case 1:
+                // dice ='You rolled One'
+                this.setState({dice1: 'You Rolled 1'})
+            break;
+            case 2:
+                dice = 2;
+            break;
+            case 3:
+                dice = 3;
+            break;
+            case 4:
+                dice = 4;
+            break;
+            case 5:
+                dice = 5;
+            break;
+            case 6:
+                dice = 6;
+            break;
+            default:
+                console.log('Roll again');
+
+        }
+        console.log('you Rolled '+ dice);
     }
     render() {
         return (
