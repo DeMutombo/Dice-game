4947be17b4d56675ca5b1ba4525b4ecfb71a4d8f
Mutombo <mgtshomba@gmail.com>
2020-11-16 15:08:29 +0200

    Roll dice function

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -8,16 +8,26 @@ import Die from './Die'
              dice1 : 1,
              dice2 : 6
          }
-     }
+    }
+
+    rollDice = ()=>{
+        // Generate a random number for the first and second dice 1-6
+        let diceNum1 = Math.floor(Math.random() * 6) + 1
+        let diceNum2 = Math.floor(Math.random() * 6) + 1
+        // Set the first dice to the generated number
+        this.setState({dice1: diceNum1})
+        this.setState({dice2: diceNum2})
+        console.log(this.state.dice1);
+    }
     render() {
         return (
             <div>
                 <div style={{display:'flex', justifyContent:'center'}}>
-                    <Die />
-                    <Die />
+                    <Die dice1={this.state.dice1}/>
+                    <Die dice1={this.state.dice2}/>
                 </div>
                              
-                <button>Roll Dice</button>
+                <button onClick={this.rollDice}>Roll Dice</button>
             </div>
         )
     }
